34ed2b6c39a6818380868328606b90b4ddbd3e29
xsSplater - СанСаныч <xssplater@gmail.com>
2025-03-30 02:05:12 +0300

    Update TransparentShield_localization.lua

--- scripts/TransparentShield_localization.lua
+++ scripts/TransparentShield_localization.lua
@@ -11,33 +11,43 @@ return {
   mod_name =
   {
     en = "Transparent Shield",
+    ru = "Прозрачный щит",
   },
   mod_description =
   {
     en = "Makes Ogryn shield transparent",
+    ru = "Transparent Shield - делает щит огрина прозрачным",
   },
   [SettingNames.EnableMod] = {
-    en = "Enable Transparency"
+    en = "Enable Transparency",
+    ru = "Включить прозрачность",
   },
   [SettingNames.Opacity] = {
-    en = "Opacity"
+    en = "Opacity",
+    ru = "Прозрачность",
   },
   [SettingNames.BlockOpacity] = {
-    en = "Opacity while blocking"
+    en = "Opacity while blocking",
+    ru = "Прозрачность при блокировании",
   },
   [SettingNames.EnableForAllItems] = {
-    en = "Enable for all items"
+    en = "Enable for all items",
+    ru = "Включить для всех предметов",
   },
   [SettingNames.OtherPlayersGroup] = {
-    en = "Settings for other players"
+    en = "Settings for other players",
+    ru = "Настройки для других игроков",
   },
   [SettingNames.EnableForOtherPlayers] = {
-    en = "Enable for other players"
+    en = "Enable for other players",
+    ru = "Включить  для других игроков",
   },
   [SettingNames.OpacityForOthers] = {
-    en = "Opacity for other players"
+    en = "Opacity for other players",
+    ru = "Прозрачность для других игроков",
   },
   [SettingNames.BlockOpacityForOthers] = {
-    en = "Opacity while blocking for other players"
+    en = "Opacity while blocking for other players",
+    ru = "Прозрачность при блокировании для других игроков",
   },
 }
